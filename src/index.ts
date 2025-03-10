38f4d02b2b91797e9e240aaaaba7d357d269e858
luuhuy2402 <xeoxeo1895@gmail.com>
2025-03-10 14:40:10 +0700

    Routing (2) tách file

--- src/index.ts
+++ src/index.ts
@@ -1,29 +1,15 @@
 import express from 'express'
-const router = express.Router()
+import userrouter from '~/user.router'
+
 const app = express()
 const port = 3000
 
 app.get('/', (req, res) => {
   res.send('Hello World!')
 })
-// đây là middleware  khi truy cập vào router ví dụ /tweets thì sẽ luôn đi qua middleware
-router.use((req, res, next) => {
-  console.log('Time:', Date.now())
-  next()
-})
 
-router.get('/tweets', (req, res) => {
-  res.json({
-    data: [
-      {
-        id: 1,
-        text: 'hello'
-      }
-    ]
-  })
-})
 //nếu muốn truy cập vào router/tweets thì sẽ cần ghi /apis/tweets
-app.use('/apis', router)
+app.use('/user', userrouter)
 
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`)
