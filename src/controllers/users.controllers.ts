1ba4dcc09fd948aed97677a43248c46a490fab8d
luuhuy2402 <xeoxeo1895@gmail.com>
2025-03-20 23:37:37 +0700

    Use checkschema to validate body register & check the email exist

--- src/controllers/users.controllers.ts
+++ src/controllers/users.controllers.ts
@@ -1,6 +1,6 @@
 //xử lý logic
-
 import { Request, Response } from 'express'
+import usersService from '~/services/users.services'
 export const loginController = (req: Request, res: Response) => {
   const { email, password } = req.body
   if (email === 'xeoxeo1895@gmail.com' && password === '123456') {
@@ -12,4 +12,22 @@ export const loginController = (req: Request, res: Response) => {
   res.status(400).json({
     error: 'Login failed'
   })
+}
+
+export const registerController = async (req: Request, res: Response) => {
+  const { email, password } = req.body
+  try {
+    const result = await usersService.register({ email, password })
+
+    res.json({
+      message: 'Register success',
+      result: result
+    })
+    return
+  } catch (error) {
+    res.status(400).json({
+      error: 'Register failed'
+    })
+    return
+  }
 }
--- src/routes/users.routes.ts
+++ src/routes/users.routes.ts
@@ -1,8 +1,16 @@
 import { Router } from 'express'
-import { loginController } from '~/controllers/users.controllers'
-import { loginValidator } from '~/middlewares/users.middlewares'
+import { loginController, registerController } from '~/controllers/users.controllers'
+import { loginValidator, registerValidator } from '~/middlewares/users.middlewares'
+
 const usersrouter = Router()
 
 usersrouter.post('/login', loginValidator, loginController)
+/**
+ * Description: Register a new user
+ * Path:/register
+ * Method: POST
+ * Body: {name: string, email: string, password: string, confirm_password: string, date_of_birth: ISO8601}
+ */
+usersrouter.post('/register', registerValidator, registerController)
 
 export default usersrouter
