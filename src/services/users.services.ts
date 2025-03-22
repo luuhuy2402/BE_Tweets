21f31f1891222ebe7150c8e8c2b38adc955e7a88
luuhuy2402 <xeoxeo1895@gmail.com>
2025-03-22 15:41:44 +0700

    Tạo Access Token và Refresh Token

--- src/services/users.services.ts
+++ src/services/users.services.ts
@@ -1,14 +1,38 @@
+import { TokenType } from '~/constants/enums'
 import { RegisterReqBody } from '~/models/requests/User.requests'
 import User from '~/models/schemas/User.schema'
 import databaseService from '~/services/database.services'
+import { hashPassword } from '~/utils/crypto'
+import { signToken } from '~/utils/jwt'
 
 class UsersService {
+  private signAccessToken(user_id: string) {
+    return signToken({
+      payload: { user_id, token_type: TokenType.AccessToken },
+      options: {
+        expiresIn: process.env.ACCESS_TOKEN_EXPIRES_IN
+      }
+    })
+  }
+
+  private signRefreshToken(user_id: string) {
+    return signToken({
+      payload: { user_id, token_type: TokenType.RefreshToken },
+      options: { expiresIn: process.env.REFRESH_TOKEN_EXPIRES_IN }
+    })
+  }
   async register(payload: RegisterReqBody) {
     const result = await databaseService.users.insertOne(
       //UserType cura date_of_birth quy  định là kiểu Date mà người dùng gửi lên kiểu string nên cần ép kiểu dữ liệu ghi đè lại thành kiểu Date
-      new User({ ...payload, date_of_birth: new Date(payload.date_of_birth) })
+      new User({ ...payload, date_of_birth: new Date(payload.date_of_birth), password: hashPassword(payload.password) })
     )
-    return result
+    const user_id = result.insertedId.toString()
+    const [accessToken, refreshToken] = await Promise.all([
+      this.signAccessToken(user_id),
+      this.signRefreshToken(user_id)
+    ])
+
+    return { accessToken, refreshToken }
   }
 
   async checkEmailExist(email: string) {
